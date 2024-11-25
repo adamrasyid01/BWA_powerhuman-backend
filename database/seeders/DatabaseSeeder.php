e63d5204420703a341a39734b0aac8018ad378a7
adamrasyid01 <adamrasyid01@gmail.com>
2024-11-25 17:47:13 +0700

    fix: make Factory (masih error)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,11 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Company;
+use App\Models\Employee;
+use App\Models\Responsibility;
+use App\Models\Role;
+use App\Models\Team;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +17,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        // Company::factory(10)->create();
+
+        // Team::factory(10)->create();
+
+        // Role::factory(50)->create();
+
+        // Responsibility::factory(100)->create();
+
+        Employee::factory(1000)->create();
     }
 }
